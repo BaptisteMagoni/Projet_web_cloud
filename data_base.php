a2ab74cb203dfb5a58a4a5a5b7b2cb428bb267b1
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-21 22:10:44 +0100

    add profil.php

--- data_base.php
+++ data_base.php
@@ -1,7 +1,7 @@
 <?php 
 	try{
 
-	$bdd = new PDO('mysql:host=127.0.0.1;dbname=bdd_web;charset=utf8', 'root', 'batmantiti44');
+	$bdd = new PDO('mysql:host=192.168.1.37;port=5432;dbname=bdd_web;charset=utf8', 'baptiste', 'titibaba44');
 
 	}catch(Exception $e){
 

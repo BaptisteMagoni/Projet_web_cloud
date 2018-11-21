e2d70dfe0202a984d325c72c297629e9addb7052
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-21 22:47:47 +0100

    data_base change

--- data_base.php
+++ data_base.php
@@ -1,17 +1,11 @@
-<?php 
-	$list_ip = array('127.0.0.1', '192.168.1.37');
+<?php
 	try{
-		foreach($list_ip as $ip){
-			try{
-				$bdd = new PDO('mysql:host='.$ip.':3306;dbname=bdd_web;charset=utf8', 'Baptiste', 'titibaba44');
-				break;
-			}catch(Exception $e){
-
-			}
-		}
-
+		$bdd = new PDO('mysql:host=192.168.1.37:3306;dbname=bdd_web;charset=utf8', 'Baptiste', 'titibaba44');
 	}catch(Exception $e){
 
-		echo "Une erreur est survenu à la connexion de la base de donnée ! \n".$e->getMessage();
+		if($e->getMessage() == "SQLSTATE[HY000] [2003] Can't connect to MySQL server on '192.168.1.37' (111)")
+			$bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=bdd_web;charset=utf8', 'Baptiste', 'titibaba44');
+		else
+			echo "Une erreur est survenu à la connexion de la base de donnée ! \n".$e->getMessage();
 
 }

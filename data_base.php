3da10ecfba5b5f35cb8ec2822e421dae808dfc05
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-21 20:28:47 +0100

    Update data_base.php

--- data_base.php
+++ data_base.php
@@ -1,10 +1,10 @@
 <?php 
 	try{
 
-	$bdd = new PDO('mysql:host=192.168.1.37;dbname=web_bdd;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=127.0.0.1;dbname=bdd_web;charset=utf8', 'root', 'batmantiti44');
 
 	}catch(Exception $e){
 
-		echo "Une erreur est survenu à la connexion de la base de donnée ! \n";
+		echo "Une erreur est survenu à la connexion de la base de donnée ! \n".$e->getMessage();
 
 }

6861adc3501cdc7b12e84e4dfcfa1d9cf97856c0
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-21 20:03:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php 
+if(isset($_POST['btn_connexion'])){
+	$id = $_POST['id'];
+	$password = $_POST['passwd'];
+	if(!empty($id) || !empty($password)){
+		echo $id." : ".$password;
+	}else{
+		$erreur = "Il manque une information au formulaire de connexion. Ressayer !";
+	}
+}
+?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
@@ -12,24 +23,24 @@
   	</head>
 	<body id="LoginForm">
 		<div class="container">
-		<h1 class="form-heading">Sevrveur Magoni</h1>
+		<h1 class="form-heading">Serveur Magoni</h1>
 			<div class="login-form">
 				<div class="main-div">
 				    <div class="panel">
 					   	<h2>Panel de connexion</h2>
 					   	<p>Entrez votre identifiant et votre mot de passe</p>
 				   	</div>
-				    <form id="Login" method="POST">
+				    <form method="POST">
 				    	<div class="form-group">
-				            <input type="email" class="form-control" id="inputEmail" placeholder="Email Address">
+				            <input type="text" name="id" class="form-control" placeholder="Identifiant">
 				        </div>
 				        <div class="form-group">
-				            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+				            <input type="password" name="passwd" class="form-control" id="inputPassword" placeholder="Password">
 				        </div>
 				        <div class="forgot">
 				        	<a href="reset.html">Mot de passe perdu?</a>
 						</div>
-				        <button type="submit" class="btn btn-primary">Connexion</button>
+				        <button type="submit" name="btn_connexion" class="btn btn-primary">Connexion</button>
 				    </form>
 				</div>
 			</div>

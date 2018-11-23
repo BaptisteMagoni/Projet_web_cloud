c0de98b0fa3f91351973f229c811e40394fb86d9
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-23 17:07:36 +0100

    b

--- Profil/comptes.php
+++ Profil/comptes.php
@@ -99,8 +99,8 @@
                   <th scope="col">Grade</th>
                 </tr>
               </thead>
-              <tbody>
               <?php while($m = $req_users->fecth()){?>
+              <tbody>
                 <tr>
                   <th scope="row"><?=$m['id']?></th>
                   <th scope="row"><?=$m['username']?></th>
@@ -108,8 +108,8 @@
                   <th scope="row"><?=$m['prenom']?></th>
                   <th scope="row"><?=$m['grade']?></th>
                 </tr>
-              <?php } ?>
               </tbody>
+              <?php } ?>
             </table>
           </div>
         <!-- Sticky Footer -->

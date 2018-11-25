3260f0f2e0422f321e687a146707e4c03694a35c
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-25 02:04:35 +0100

    Update data_base.php

--- data_base.php
+++ data_base.php
@@ -3,7 +3,7 @@
 		$bdd = new PDO('mysql:host=176.132.180.249:3306;dbname=bdd_web;charset=utf8', 'Baptiste', 'titibaba44');
 	}catch(Exception $e){
 
-		if($e->getMessage() == "SQLSTATE[HY000] [2005] Unknown MySQL server host '192.168.1.37:3306' (2)")
+		if($e->getMessage() == "SQLSTATE[HY000] [2005] Unknown MySQL server host '176.132.180.249:3306' (2)")
 			$bdd = new PDO('mysql:host=127.0.0.1;dbname=bdd_web;charset=utf8', 'Baptiste', 'titibaba44');
 		else
 			echo "Une erreur est survenu à la connexion de la base de donnée ! ".$e->getMessage();
